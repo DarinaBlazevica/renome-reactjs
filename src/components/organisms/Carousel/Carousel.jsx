5655fc86dea23c856c858fb7428f56aa040d6dd7
Darina <darina.blazevica@accenture.com>
2022-03-31 19:00:59 +0300

    styling carousel changed,had problems with page

--- src/components/organisms/Carousel/Carousel.jsx
+++ src/components/organisms/Carousel/Carousel.jsx
@@ -7,7 +7,9 @@ const Carousel = (props) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [imageDirection, setImageDirection] = useState("slide-right");
   const [textDirection, setTextDirection] = useState("slide_text-right");
-  let styleLast = "carousel-container__text .subheading--oranges";
+  const [position, setPosition] = useState(
+    "carousel__container__carousel-image--active"
+  );
 
   const slidePrev = () => {
     const nextIndex = activeIndex - 1;
@@ -18,12 +20,14 @@ const Carousel = (props) => {
     }
     setImageDirection("slide-left");
     setTextDirection("slide_text-left");
+    setPosition("carousel__container__carousel-image--active");
   };
 
   const slideNext = () => {
     setActiveIndex((activeIndex + 1) % props.carousel.length);
     setImageDirection("slide-right");
     setTextDirection("slide_text-right");
+    setPosition("carousel__container__carousel-image--active");
   };
 
   const cloneImage = (imageDirection) => (child) =>
@@ -38,46 +42,55 @@ const Carousel = (props) => {
 
   return (
     <div className="carousel-container">
-      <div className="carousel-container__carousel">
-        <TransitionGroup childFactory={cloneImage(imageDirection)}>
-          <CSSTransition
-            key={props.carousel[activeIndex].path}
-            timeout={1000}
-            classNames={imageDirection}
-          >
-            <img src={props.carousel[activeIndex].path} alt="carousel" />
-          </CSSTransition>
-        </TransitionGroup>
-      </div>
-      <div className="carousel-container__text">
-        <TransitionGroup childFactory={cloneText(textDirection)}>
-          <CSSTransition
-            key={props.carousel[activeIndex].title}
-            timeout={1000}
-            classNames={textDirection}
-          >
-            <h1 className={props.carousel[activeIndex].title ===
-                props.carousel[2].title
-                  ? "heading--oranges"
-                  : "heading"} >{props.carousel[activeIndex].title}</h1>
-          </CSSTransition>
-          <CSSTransition
-            key={props.carousel[activeIndex].subTitle}
-            timeout={1000}
-            classNames={textDirection}
-          >
-            <h2
-              className={
-                props.carousel[activeIndex].subTitle ===
-                props.carousel[2].subTitle
-                  ? "subheading--oranges"
-                  : "subheading"
-              }
+      <div className="carousel-container__slider">
+        <div className="carousel-container__carousel">
+          <TransitionGroup childFactory={cloneImage(imageDirection)}>
+            <CSSTransition
+              key={props.carousel[activeIndex].path}
+              timeout={1000}
+              classNames={imageDirection}
             >
-              {props.carousel[activeIndex].subTitle}
-            </h2>
-          </CSSTransition>
-        </TransitionGroup>
+              <div className="carousel-container__slider">
+                <img src={props.carousel[activeIndex].path} alt="carousel"/>
+              </div>
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
+        <div className="carousel-container__text">
+          <TransitionGroup childFactory={cloneText(textDirection)}>
+            <CSSTransition
+              key={props.carousel[activeIndex].title}
+              timeout={1000}
+              classNames={textDirection}
+            >
+              <h1
+                className={
+                  props.carousel[activeIndex].title === props.carousel[2].title
+                    ? "heading--oranges"
+                    : "heading"
+                }
+              >
+                {props.carousel[activeIndex].title}
+              </h1>
+            </CSSTransition>
+            <CSSTransition
+              key={props.carousel[activeIndex].subTitle}
+              timeout={1000}
+              classNames={textDirection}
+            >
+              <h2
+                className={
+                  props.carousel[activeIndex].subTitle ===
+                  props.carousel[2].subTitle
+                    ? "subheading--oranges"
+                    : "subheading"
+                }
+              >
+                {props.carousel[activeIndex].subTitle}
+              </h2>
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
       </div>
       <Arrow slideNext={() => slideNext()} slidePrev={() => slidePrev()} />
     </div>
